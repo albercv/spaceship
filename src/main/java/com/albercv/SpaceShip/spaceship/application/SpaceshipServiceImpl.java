992e6c2efa11728a27a94913bd8b74a20297c81d
alberto carrasco <albercv@MacBook-Pro-de-alberto.local>
2024-10-18 20:27:39 +0200

    Adds redis cache

--- src/main/java/com/albercv/SpaceShip/spaceship/application/SpaceshipServiceImpl.java
+++ src/main/java/com/albercv/SpaceShip/spaceship/application/SpaceshipServiceImpl.java
@@ -3,6 +3,8 @@ package com.albercv.SpaceShip.spaceship.application;
 import com.albercv.SpaceShip.spaceship.application.port.SpaceshipService;
 import com.albercv.SpaceShip.spaceship.domain.Spaceship;
 import com.albercv.SpaceShip.spaceship.domain.port.SpaceshipRepository;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,22 +19,26 @@ public class SpaceshipServiceImpl implements SpaceshipService {
         this.spaceshipRepository = spaceshipRepository;
     }
 
+    @Cacheable(value = "spaceships", key = "#root.args[0]")
     @Override
     public Page<Spaceship> getAllSpaceships(Optional<String> name, Pageable pageable) {
         return name.map(n -> spaceshipRepository.findByNameContainingIgnoreCase(n, pageable))
                 .orElseGet(() -> spaceshipRepository.findAll(pageable));
     }
 
+    @Cacheable(value = "spaceships", key = "#id")
     @Override
     public Optional<Spaceship> getSpaceshipById(Long id) {
         return spaceshipRepository.findById(id);
     }
 
+    @CacheEvict(value = "spaceships", allEntries = true)
     @Override
     public Spaceship createSpaceship(Spaceship spaceship) {
         return spaceshipRepository.save(spaceship);
     }
 
+    @CacheEvict(value = "spaceships", allEntries = true)
     @Override
     public Spaceship updateSpaceship(Long id, Spaceship spaceship) {
         if (spaceshipRepository.findById(id).isPresent()) {
@@ -43,6 +49,7 @@ public class SpaceshipServiceImpl implements SpaceshipService {
         }
     }
 
+    @CacheEvict(value = "spaceships", allEntries = true)
     @Override
     public void deleteSpaceship(Long id) {
         spaceshipRepository.deleteById(id);
